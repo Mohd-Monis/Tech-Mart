f1d2a3b7a3037a6220c3c22fe7d7bb0bdc198976
Monis <monisdl.2005@gmail.com>
2024-07-06 13:46:44 +0530

    done

--- controllers/session-controllers.js
+++ controllers/session-controllers.js
@@ -1,13 +1,10 @@
 const session = require("express-session");
 const mongoDBStore = require("connect-mongodb-session")(session);
 
-const uri  = proces.env.MONGO_URI;
-
 
 const uri = process.env.MONGO_URI;
 function createSession(){
     const store = mongoDBStore({
-        uri:uri,
         uri:uri,
         databaseName:"Shop",
         collection:"session"
